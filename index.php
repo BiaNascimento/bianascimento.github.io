6b567848a1cceb10e0a8a7fbba2bdcb0b4e1a616
Gabriel Felippi P. Baragate <gabrielbaragate1@gmail.com>
2021-04-23 00:23:26 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,11 +1,12 @@
-<?php
-	if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
-		$uri = 'https://';
-	} else {
-		$uri = 'http://';
-	}
-	$uri .= $_SERVER['HTTP_HOST'];
-	header('Location: '.$uri.'/dashboard/');
-	exit;
-?>
-Something is wrong with the XAMPP installation :-(
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php echo date('D M Y');?>
+</body>
+</html>
